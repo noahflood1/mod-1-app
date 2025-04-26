5072eab9f8da95498fde7227e8190f6aafac5a5b
Abhishek Jariwala <119805313+ajariwala1@users.noreply.github.com>
2025-04-26 07:13:42 -0500

    Initial Commit

---
+++ au hello world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+// ViewController.swift : au hello world
+//
+// Copyright © 2025 Auburn University.
+// All Rights Reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
